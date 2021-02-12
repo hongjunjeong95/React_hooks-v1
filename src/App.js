07d4c2dd4d2bd7b0189727f0499e08fefaaac693
zpskek <wjdghdwns0@gmail.com>
2021-02-12 11:14:01 +0900

    useNotification

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import { useFullscreen } from "./useFullscreen";
+import { useNotification } from "./useNotification";
 
 const App = () => {
-  const onFulls = (isFull) => {
-    console.log(isFull ? "We are full" : "We are small");
-  };
-  const { element, triggerFull, exitFull } = useFullscreen(onFulls);
+  const triggerNotif = useNotification("Can I steal your kimchi?", {
+    body: "I love kimchi don't you?",
+  });
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <div ref={element}>
-        <img src="https://images.unsplash.com/photo-1549911511-d65069d34e45?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80&width=400&height=600"></img>
-        <button onClick={exitFull}>Exit fullscreen</button>
-      </div>
-      <button onClick={triggerFull}>Make fullscreen</button>
+      <button onClick={triggerNotif}>Hello</button>
     </div>
   );
 };
